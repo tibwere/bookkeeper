bb028e760ad12551c5b01ed1ec11d378d634f13b
Simone Tiberi <tib.sim.98@gmail.com>
2022-05-09 00:04:09 +0200

    Added a test for NetworkTopologyImpl#chooseRandom

--- bookkeeper-server/src/test/java/it/uniroma2/dicii/isw2/bookkeeper/net/NetworkTopologyImplTest.java
+++ bookkeeper-server/src/test/java/it/uniroma2/dicii/isw2/bookkeeper/net/NetworkTopologyImplTest.java
@@ -8,8 +8,10 @@ import org.junit.runners.Parameterized;
 
 import org.junit.Assert;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @RunWith(value = Enclosed.class)
 public class NetworkTopologyImplTest {
@@ -39,8 +41,8 @@ public class NetworkTopologyImplTest {
             String validName = "test-node";
             String validLocation = NodeBase.PATH_SEPARATOR_STR + "test-loc";
 
-            String invalidName = "";
-            String invalidLocation = "";
+            String invalidName = "29314";
+            String invalidLocation = "loc-without-separator";
 
             String illArgExc = "java.lang.IllegalArgumentException";
             String nilPtrExc = "java.lang.NullPointerException";
@@ -95,6 +97,70 @@ public class NetworkTopologyImplTest {
                     }
                 }
             }
+        }
+    }
+
+    @RunWith(value = Parameterized.class)
+    public static class ChooseRandomTests {
+
+        private static final char NOT_INDICATOR = '~';
+
+        private NetworkTopologyImpl nt;
+        private String scope;
+
+        public ChooseRandomTests(List<Node> nodes, String scope) {
+            configure(nodes, scope);
+        }
+
+        public void configure(List<Node> nodes, String scope) {
+            this.nt = new NetworkTopologyImpl();
+            nodes.forEach(nt::add);
+            this.scope = scope;
+        }
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> testCasesTuples() {
+
+            try {
+                String scope = NodeBase.PATH_SEPARATOR_STR + "rack-good";
+                String[] others = new String[]{
+                        NodeBase.PATH_SEPARATOR_STR + "other-rack-1",
+                        NodeBase.PATH_SEPARATOR_STR + "other-rack-2"
+                };
+
+                List<Node> nodes = new ArrayList<>(Arrays.asList(
+                        new BookieNode(BookieId.parse("test0"), scope),
+                        new BookieNode(BookieId.parse("test1"), scope),
+                        new BookieNode(BookieId.parse("test2"), others[0]),
+                        new BookieNode(BookieId.parse("test3"), others[1])
+                ));
+
+                return Arrays.asList(new Object[][]{
+                        {nodes, scope},
+                        {nodes, NOT_INDICATOR + scope},
+                        {nodes, ""}
+                });
+
+            } catch (Exception e) {
+                Assert.fail("These tests assume valid nodes are used");
+                return null;
+            }
+        }
+
+        /**
+         * testChooseRandom - after building a certain topology composed
+         * of nodes specified as parameters, a node is extracted at random
+         * and its membership is verified
+         */
+        @Test
+        public void testChooseRandom() {
+            Node chosen = this.nt.chooseRandom(this.scope);
+
+            if (this.scope.length() > 1 && this.scope.charAt(0) == NOT_INDICATOR)
+                Assert.assertFalse(chosen.getNetworkLocation().startsWith(this.scope));
+            else
+                Assert.assertTrue(chosen.getNetworkLocation().startsWith(this.scope));
         }
     }
 }
